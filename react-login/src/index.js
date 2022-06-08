36988eef072f5c6a2ef212ad86fce866c94945cc
John Eleanor <mrs.ch64@gmail.com>
2022-06-08 23:52:18 +0700

    [+] Add Album

--- react-login/src/index.js
+++ react-login/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Login from './Login';
+import Album from './Album';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -14,6 +15,7 @@ root.render(
   <Routes>
     <Route path="/" element={<App />} />
     <Route path="/login" element={<Login />} />
+    <Route path="/album" element={<Album />} />
   </Routes>
   </BrowserRouter>
 );
